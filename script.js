a0d459dc12e666add68985331840245222c0af58
MarisMihailovs <marismihailovs@gmail.com>
2023-02-16 16:39:35 +0200

    on scroll animation added

--- script.js
+++ script.js
@@ -6,6 +6,55 @@ const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
 const navItems = [nav1, nav2, nav3, nav4, nav5];
+const sectionHeadings = document.querySelectorAll('.sectionHeading');
+
+window.addEventListener('scroll', () => {
+    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+    console.log("window.pageYOffset", window.pageYOffset);
+    console.log("document.body.offsetHeight", document.body.offsetHeight);
+    console.log("window.innerHeight", window.innerHeight);
+
+}, false);
+
+
+
+// on scroll animation for headings
+
+let callback = (entries, observer) => {
+    // target element:
+    //   entry.boundingClientRect
+    //   entry.intersectionRatio
+    //   entry.intersectionRect
+    //   entry.isIntersecting
+    //   entry.rootBounds
+    //   entry.target
+    //   entry.time
+    entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0) {
+            console.log("entry.boundingClientRect", entry.boundingClientRect);
+            // Add 'show-heading' class if observation target is inside viewport
+            entry.target.classList.add('show-heading');
+        } else {
+            // Remove 'show-heading' class otherwise
+            entry.target.classList.remove('show-heading');
+        }
+    })
+}
+
+const options = {
+    // root: null,
+    // rootMargin: '0px',
+    // threshold: 1.0
+};
+
+let observer = new IntersectionObserver(callback, options);
+sectionHeadings.forEach(el => observer.observe(el));
+
+
+
+
+
+
 
 // nav animation function
 
@@ -43,3 +92,4 @@ menuBars.addEventListener('click', toggleNav);
 navItems.forEach((nav) => {
     nav.addEventListener('click', toggleNav);
 })
+
